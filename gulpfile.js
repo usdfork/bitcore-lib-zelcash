89d10208861fa63821942d2d59dba41156306917
bitzec <bitzec18@gmail.com>
2019-08-05 23:48:24 +0200

    Revert "zelcash"
    This reverts commit 162a17262ceec2cc4c7b41c8d806e2de2827bace.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 
 
-var bitcoreTasks = require('bitcore-build-zelcash');
+var bitcoreTasks = require('bitcore-build-zcash');
 
 bitcoreTasks('lib');
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib-zelcash found. ' +
-      'Please make sure to require bitcore-lib-zelcash and check that submodules do' +
-      ' not also include their own bitcore-lib-zelcash dependency.';
+    var message = 'More than one instance of bitcore-lib-zcash found. ' +
+      'Please make sure to require bitcore-lib-zcash and check that submodules do' +
+      ' not also include their own bitcore-lib-zcash dependency.';
     // TODO: put this back if we start versioning again
     //throw new Error(message);
   }
